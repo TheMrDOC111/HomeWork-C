fdd8d26b4c644cd76667ec93232671d0da95ee5f
TheMrDOC111 <themrdoc1111@mail.ru>
2020-06-01 16:44:16 +0300

    Добавлена вторая стратегия

--- HomeWork4/main.cpp
+++ HomeWork4/main.cpp
@@ -2,13 +2,15 @@
 #include "game/game.h"
 #include "strategies/human.h"
 #include "strategies/computer.h"
+#include "strategies/computer2.h"
+
 
 
 int main() {
   auto player1 = std::make_shared<computer_strategy_t>("First player");
-  auto player2 = std::make_shared<computer_strategy_t>("Second player");
+  auto player2 = std::make_shared<computer_strategy_second_t>("Second player");
 
-  for (int i = 0; i < 50; ++i) {
+  for (int i = 0; i < 30; ++i) {
     game_t game(player1, player2);
     game.play();
   }
